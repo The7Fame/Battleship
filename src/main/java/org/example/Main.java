10c2420758d837ab649eaed5ba9b4d5f9004e016
DD <sdsd>
2024-02-06 23:00:41 +0300

    add some logic to automatically set up fields

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
 import org.example.game.Game;
-import org.example.ship.*;
-
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
